c7842ee46033f591b425f718b624a777b17536aa
Nadeem Ahmed <nadeem_ahmed94@hotmail.com>
2016-02-23 13:34:21 +0000

    Amazon credentials added back to argos_model.php
    Amazon feature will now work as intended again.

--- application/models/argos_model.php
+++ application/models/argos_model.php
@@ -12,9 +12,9 @@ class argos_model extends CI_Model
 	function getDeals()
 	{
 		// My unique identifiers that are required for amazon requests.
-		$AWSAccessKeyId = 'removed1';
-		$AWSSecretKey = 'removed2';
-		$AssociatesID = 'removed3';
+		$AWSAccessKeyId = 'AKIAJIKILE6E2N5WBNNA';
+		$AWSSecretKey = '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';
+		$AssociatesID = 'argosdealscom-21';
 		
 		// Arguments for Amazon request.
 		$args = array("Operation"=>"ItemSearch","SearchIndex"=>"All","ResponseGroup"=>"Offers","MerchantId"=>"All",
